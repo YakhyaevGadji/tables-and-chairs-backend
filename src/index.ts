54a8383040010cae9f7e0a86296ff9dbfdc9f650
gadji <gadji180105@gmail.com>
2026-01-07 14:48:28 +0300

    start

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+
+const expressApp = express();
+
+expressApp.get("/", (req: express.Request, res: express.Response) => {
+    res.json({message: "hello world"})
+});
+
+expressApp.listen(3000, () => {
+    console.log("Server is running on port 3000");
+})
