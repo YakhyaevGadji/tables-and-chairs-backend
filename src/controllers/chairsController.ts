b4c9805cc13fbc534902d39e59215cf65fa67c18
gadji <gadji180105@gmail.com>
2026-01-08 16:28:33 +0300

    fix: ubuntu

--- src/controllers/chairsController.ts
+++ src/controllers/chairsController.ts
@@ -1,18 +1,22 @@
 import express from "express";
-import ApiError from "../error/ApiError";
+
+import {Chairs} from "../models/models";
 
 export const getAll = async (
     req: express.Request,
     res: express.Response,
     next: express.NextFunction
 ) => {
-    const {id} = req.query;
-
-    if(!id) {
-        return next(ApiError.badRequest("lox"))
-    }
+    res.json({message: 'test'});
 }
 
-// export const create = async (req: express.Request, res: express.Response, next: any) => {
-//
-// }
+export const create = async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+) => {
+    const data = req.body;
+    const chair = await Chairs.create(data);
+
+    return res.json(chair)
+}
--- src/routes/chairsRouter.ts
+++ src/routes/chairsRouter.ts
@@ -1,8 +1,8 @@
 import express, { Router } from "express";
-import { getAll } from "../controllers/chairsController";
-const router = Router();
+import {create, getAll} from "../controllers/chairsController";
+const router = Router()
 
 router.get('/', getAll);
-router.post('/create', )
+router.post('/create', create);
 
 export default router;
