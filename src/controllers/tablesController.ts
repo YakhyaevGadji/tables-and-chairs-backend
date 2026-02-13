addfc4ae660201b3d8d9f6ea516bd3e97db0fb06
gadji <gadji180105@gmail.com>
2026-02-13 19:03:33 +0300

    test

--- src/controllers/tablesController.ts
+++ src/controllers/tablesController.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import {Tables} from "../models/models";
+import {StatusCodes} from "http-status-codes";
 
 export const getAllTables = async (
     req: express.Request,
@@ -16,5 +17,20 @@ export const postTables = async (
     res: express.Response,
     next: express.NextFunction
 ) => {
+    try {
+        if (!req.body.username) {
+            return res.status(StatusCodes.BAD_REQUEST).json({
+                message: "username is required"
+            });
+        }
 
+        return res.status(StatusCodes.OK).json({
+            username: req.body.username
+        });
+
+    } catch (err: any) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            message: err.message
+        });
+    }
 }
